6e291de48e7802f909f7cd76b1b268d729339076
Baizid MD Ashadzzaman <86706204+Baizidmdashadzzaman@users.noreply.github.com>
2023-08-08 19:41:31 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import GlassesTryingOn from './components/glasses/GlassesTryingOn';
+
+import CarsCustomizing from './components/cars/CarsCustomizing';
+
+function App() {
+  return (
+    <div>
+      <GlassesTryingOn />
+      <CarsCustomizing />
+    </div>
+  );
+}
+
+export default App;
